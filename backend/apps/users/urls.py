13f62783176aee28a7ec3ab7adabd2afa9efe016
UltrixwOw <ultrixwow@gmail.com>
2026-02-05 21:32:35 +0100

    add serializers.py, fix urls.py

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path
 from .views import RegisterView, VerifyEmailView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
