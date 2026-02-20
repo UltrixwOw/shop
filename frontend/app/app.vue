6353202b266d6f115cc1083927c22af7d5a41d5f
UltrixwOw <ultrixwow@gmail.com>
2026-02-21 00:17:28 +0100

    install nuxt 4

---
+++ frontend/app/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    <pre>{{ products }}</pre>
+  </div>
+</template>
+
+<script setup>
+// useAsyncData или useFetch автоматически выполняются на сервере
+const config = useRuntimeConfig();
+
+// Лучший способ для Nuxt - useFetch
+const { data: products, error } = await useFetch(`${config.public.apiBase}/shop/products/`, {
+  // Опции запроса
+  server: true, // выполнять на сервере (по умолчанию true)
+  lazy: false, // ждать завершения
+});
+</script>
